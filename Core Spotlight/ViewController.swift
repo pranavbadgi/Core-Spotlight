59f381831651f6c6eab895e13e4d0442e36beb6b
Pranav Badgi <pranav.badgi@ringcentral.com>
2022-06-27 14:59:07 -0700

    Initial Commit

---
+++ Core Spotlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Spotlight
+//
+//  Created by Pranav Badgi on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
